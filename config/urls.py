e6c850b5f90f8f401ace5c8e80a287e0a0df0514
Aji Pratama <atamasite@gmail.com>
2020-11-29 18:05:53 +0700

    Change dashboard urls

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,7 @@ from app.views import HomeView, WeddingView
 
 
 urlpatterns = [
-    path('the-dashboard/', admin.site.urls),
+    path('dashboard/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
     path('<str:slug>/', WeddingView.as_view(), name='wedding'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
